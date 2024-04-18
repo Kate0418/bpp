8c11d56123470012e1d1e35c8390ea185344e837
Kate0418 <142198827+Kate0418@users.noreply.github.com>
2024-04-18 14:03:29 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["52.194.21.217"]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
