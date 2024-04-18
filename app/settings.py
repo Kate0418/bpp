a73abc41c9cfa6341a10ed36fbf3297b75f0f9aa
Kate0418 <142198827+Kate0418@users.noreply.github.com>
2024-04-18 14:28:10 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -14,12 +14,12 @@ import environ
 from pathlib import Path
 
 env= environ.Env()
-env.read_env(os.path.join(BASE_DIR,'.env'))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env.read_env(os.path.join(BASE_DIR,'.env'))
 
 
 # Quick-start development settings - unsuitable for production
