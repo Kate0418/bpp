e1abbc181ba1fc51b14d2025750a307e4e66dcf3
Kate0418 <142198827+Kate0418@users.noreply.github.com>
2024-04-18 14:32:11 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'default':env.db(),
+        'default': env.db(),
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
