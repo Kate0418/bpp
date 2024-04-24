7d5b0621dfcabc34d534c2f404fa5fc328e7c192
Kate0418 <142198827+Kate0418@users.noreply.github.com>
2024-04-24 17:47:45 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -26,6 +26,7 @@ env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+ALLOWED_HOSTS=["*"]
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
